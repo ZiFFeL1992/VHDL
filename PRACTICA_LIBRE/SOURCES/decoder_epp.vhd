0f60b59bcc1b5c8f200f316cd89213da70ad61a3
Guille <darkwilly92@gmail.com>
2017-04-03 16:43:37 +0200

    Apartado3 wip

--- PRACTICA_LIBRE/SOURCES/decoder_epp.vhd
+++ PRACTICA_LIBRE/SOURCES/decoder_epp.vhd
@@ -1,15 +1,76 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity decoder_epp is
   port (
-    CLK : in  std_logic;c_vector (1 downto 0));
-end;
+    CLK       : in  std_logic;
+    RST       : in  std_logic;
+    DIR       : in  std_logic_vector(7 downto 0);
+    DATO      : in  std_logic_vector(7 downto 0);
+    DATOS_VLD : in  std_logic;
+    RESTART   : out std_logic;
+    VOL_CODE  : out std_logic_vector(4 downto 0);
+    FREC_CODE : out std_logic_vector(7 downto 0);
+    CHANEL    : out std_logic_vector(1 downto 0));
+end decoder_epp;
 
 architecture rtl of decoder_epp is
 
 begin
 
+  process(clk, rst)
+  begin
+    if rst = '1' then
+      RESTART <= '0';
+    elsif clk'event and clk = '1' then
+      if DIR = x"11" and DATO = x"11" then
+        RESTART <= '1';
+      else
+        RESTART <= '0';
+      end if;
+    end if;
+  end process ; -- restart
+
+  process(clk, rst)
+  begin
+    if rst = '1' then
+      frec_code <= (others => '0');
+    elsif clk'event and clk = '1' then
+      if datos_vld = '1' and dir = x"F0" then
+        frec_code <= dato;
+      end if ;
+    end if ;
+  end process ; -- frec_code
+
+  process(clk, rst)
+  begin
+    if rst = '1' then
+      vol_code <= (others => '0');
+    elsif clk'event and clk = '1' then
+      if datos_vld = '1' and dir = x"B0" then
+        vol_code <= dato(4 downto 0);
+      end if ;
+    end if ;
+  end process ; -- vol_code
+
+  process(clk, rst)
+  begin
+    if rst = '1' then
+      chanel <= (others => '0');
+    elsif clk'event and clk = '1' then
+      if datos_vld = '1' and dir = x"CA" then
+        case(dato) is
+          when x"DD" =>
+            chanel <= "10";
+          when x"11" =>
+            chanel <= "01";
+          when x"22" =>
+            chanel <= "11";
+          when others =>
+            chanel <= "00";
+        end case ;
+      end if ;
+    end if ;
+  end process ; -- channel
 
 end rtl;
---
+++ PRACTICA_LIBRE/SOURCES/decoder_epp_tb.vhd
@@ -0,0 +1,91 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity decoder_epp_tb is
+
+end decoder_epp_tb;
+
+
+architecture sim of decoder_epp_tb is
+
+    signal CLK_i       : std_logic := '0';
+    signal RST_i       : std_logic := '1';
+    signal DIR_i       : std_logic_vector(7 downto 0);
+    signal DATO_i      : std_logic_vector(7 downto 0);
+    signal DATOS_VLD_i : std_logic := '0';
+    signal RESTART_i   : std_logic;
+    signal VOL_CODE_i  : std_logic_vector(4 downto 0);
+    signal FREC_CODE_i : std_logic_vector(7 downto 0);
+    signal CHANEL_i    : std_logic_vector(1 downto 0);
+
+begin  -- sim
+
+  DUT : entity work.decoder_epp
+    port map (
+        CLK       => CLK_i,
+        RST       => RST_i,
+        DIR       => DIR_i,
+        DATO      => DATO_i,
+        DATOS_VLD => DATOS_VLD_i,
+        RESTART   => RESTART_i,
+        VOL_CODE  => VOL_CODE_i,
+        FREC_CODE => FREC_CODE_i,
+        CHANEL    => CHANEL_i);
+
+
+  RST_i <= '1', '0'  after 223 ns;
+  CLK_i <= not CLK_i after 5 ns;
+
+
+  process
+  begin  -- process
+
+    wait until RST_i'event and RST_i = '0';
+
+    -- Init
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '1';
+    DIR_i <= x"11";
+    DATO_i <= x"11";
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '0';
+
+    wait for 150 ns;
+
+
+    -- frec
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '1';
+    DIR_i <= x"F0";
+    DATO_i <= x"00";
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '0';
+
+    wait for 150 ns;
+
+
+    -- vol
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '1';
+    DIR_i <= x"B0";
+    DATO_i <= x"00";
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '0';
+
+    wait for 150 ns;
+
+    -- both channels
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '1';
+    DIR_i <= x"CA";
+    DATO_i <= x"22";
+    wait until CLK_i'event and CLK_i = '0';
+    DATOS_VLD_i <= '0';
+
+    wait for 150 ns;
+
+    report "FIN CONTROLADO DE LA SIMULACION" severity failure;
+  end process;
+
+
+end sim;
